f3ef6c6e1c01ee787c78b2005fdf3ac0dc7651e5
Vinicius Moreira <89.vinicius.moreira@gmail.com>
2022-05-13 11:26:50 -0300

    Estudo de strings

---
+++ exemplo-02.php
@@ -0,0 +1,15 @@
+<?php
+
+$nome = "joão rangel";
+
+echo strtoupper($nome); // string to upper - passa tudo para maiúsculo
+
+echo "<br>";
+
+$nome = strtoupper($nome);
+
+echo $nome;
+
+echo "<br>";
+
+?>
